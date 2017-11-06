0750c5dc89077936c168b87f970d7bbc4e2a8046
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-11-06 13:16:21 -0500

    working on first method

---
+++ Array Methods/src/ArrayMethods.java
@@ -0,0 +1,22 @@
+
+public class ArrayMethods {
+
+	public static void main (String[] args)
+	{
+		
+	}
+	public static int[] removeDuplicates(int[] list)
+	{
+		int[] newArr [];
+		for(int i = 0; i<list.length-1;i++)
+		{
+			for(int j = i+1;j<list.length;j++)
+			{
+				if(!(list[i] == list[j]))
+				{
+					
+				}
+			}
+		}
+	}
+}
